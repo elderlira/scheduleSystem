cba4ceca7e6b6bc757d27c765f9771b4a2a38537
Elder Lira <elderfranca@hotmail.com>
2022-04-17 11:14:26 -0300

    schedule form created

---
+++ src/components/ScheduleForm.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="home container">
+    <h1 v-text="title"></h1>
+    <div>
+      <p>
+          Inicio: <input type="date" name="" id="" v-model="inicial">
+          Fim: <input type="date" name="" id="" v-model="final">
+      </p>
+    </div>
+    <div>
+      <input type="text" name="" id="" v-model="tarefa" >       
+      <button class="add" @click="adicionarTarefa">Add</button>
+      <button class="clear" @click="limparLis">Clear</button>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'Home',
+  data() {
+    return {
+      title: "Listagem das tarefas da semana",
+      inicial: '',
+      final: '',
+      tarefas: [],
+      tarefa: '',
+      dataInicialFormata: ''
+    }
+  },
+  methods: {
+   
+  },
+  mounted() {
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+.add {
+  background: lightblue;
+  color: black;
+  font-weight: bolder;
+}
+
+.clear {
+  background: lightcoral;
+  color: white;
+  font-weight: bolder;
+}
+
+
+li {
+  list-style-type: none;
+}
+.normal {
+  color: black;
+  font-weight: bold;
+  font-size: 50px;
+}
+.ativa {
+  color: blue;
+  font-size: 40px;
+}
+</style>
