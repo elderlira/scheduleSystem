1e0dcdd3d4ad4d0130e509c67f5d8281b1b994c1
Elder Lira <elderfranca@hotmail.com>
2022-04-24 12:03:18 -0300

    implementing state and mutations

--- src/components/ScheduleForm.vue
+++ src/components/ScheduleForm.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="home container">
+    <!-- {{ dataStorage }} -->
     <h1 v-text="title"></h1>
-    <div>
+    <div class="inputs">
       <p>
-          Inicio: <input type="date" name="" id="" v-model="inicial">
-          Fim: <input type="date" name="" id="" v-model="final">
+          <span>Start:</span> <input type="date" name="" id="" v-model="inicial">
+          <span> End: </span> <input type="date" name="" id="" v-model="final">
       </p>
     </div>
     <div>
@@ -16,7 +17,7 @@
       <p class="normal">You don`t have any task</p>
     </div>
     <div v-else>
-      <p>You have: {{tarefas.length}} task from {{ inicial }} to {{ final }}</p>
+      <p>You have: {{tarefas.length}} task from {{ dataStorage.dataInicial }} to {{ dataStorage.dataFinal }}</p>
       <ol>
         <li
           :class="tarefa.length === 0 ? 'normal' : 'ativa' "
@@ -31,20 +32,25 @@
 </template>
 
 <script>
+import { mapState, mapMutations } from 'vuex'
 // @ is an alias to /src
 export default {
   name: 'Home',
   data() {
     return {
-      title: "Listagem das tarefas da semana",
-      inicial: '',
-      final: '',
+      title: "Week task list",
+      inicial: this.$store.state.dataStorage.dataInicial || '',
+      final: this.$store.state.dataStorage.dataFinal || '',
       tarefas: [],
       tarefa: '',
       dataInicialFormata: ''
     }
   },
+  computed: {
+    ...mapState(['dataStorage'])
+  },
   methods: {
+    ...mapMutations(['GET_PERIODS_DATE']),
     adicionarTarefa() {
       if(this.inicial === '' && this.final==='') {
         alert('informe o periodo da semana')
@@ -68,11 +74,12 @@ export default {
         ...this.tarefas, {
          nome: this.tarefa, 
          data: formatDate(new Date())
-        }
-         ]
+        }]
+       this.GET_PERIODS_DATE({
+        inicial: this.inicial,
+        final: this.final
+      })
       this.tarefa = ''
-      this.inicial = ''
-      this.final = ''
     },
   limparLis() {
     this.tarefas = []
@@ -86,6 +93,16 @@ export default {
 </script>
 
 <style>
+.inputs {
+  /* background: blueviolet; */
+  padding: 10px 0;
+}
+
+span {
+  font-weight: bold;
+  margin-right: 4px;
+  margin-left: 12px;
+}
 .add {
   background: lightblue;
   color: black;
@@ -105,10 +122,10 @@ li {
 .normal {
   color: black;
   font-weight: bold;
-  font-size: 50px;
+  font-size: 20px;
 }
 .ativa {
-  color: blue;
-  font-size: 40px;
+  color: rgba(39, 185, 204, 0.856);
+  font-size: 30px;
 }
 </style>
