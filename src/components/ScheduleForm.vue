178c9d3575232ec95ba4eb9429e9918919a9499a
Elder Lira <elderfranca@hotmail.com>
2022-05-13 23:39:46 -0300

    added button and methods to copy list task

--- src/components/ScheduleForm.vue
+++ src/components/ScheduleForm.vue
@@ -3,21 +3,40 @@
     <h1 v-text="title"></h1>
     <div class="inputs">
       <p>
-          <span>Start:</span> <input type="date" name="" id="" v-model="inicial">
-          <span> End: </span> <input type="date" name="" id="" v-model="final">
+        <span>Start:</span>
+        <input type="date" name="" id="" v-model="inicial" />
+        <span> End: </span> <input type="date" name="" id="" v-model="final" />
       </p>
     </div>
     <div>
-      <input type="text" name="" id="" v-model="tarefa" placeholder="Insert your task">       
-      <button class="add" @click="adicionarTarefa"><i class="bi bi-plus-circle"></i></button>
-      <button class="clear" @click="limparLis"><i class="bi bi-x-circle"></i></button>
-      <button class="clearAll" @click="clearAll"><i class="bi bi-calendar-x"></i></button>
+      <input
+        type="text"
+        name="textoTarefa"
+        id="textoTarefa"
+        v-model="tarefa"
+        placeholder="Insert your task"
+      />
+      <button class="add" @click="adicionarTarefa">
+        <i class="bi bi-plus-circle"></i>
+      </button>
+      <button class="clear" @click="limparLis">
+        <i class="bi bi-x-circle"></i>
+      </button>
+      <button class="clearAll" @click="clearAll">
+        <i class="bi bi-calendar-x"></i>
+      </button>
+      <button class="download" @click="downloadList">
+        <i class="bi bi-download"></i>
+      </button>
     </div>
-    <div v-if="tarefas.length === 0 ">
+    <div v-if="tarefas.length === 0">
       <p class="normal">You don`t have any task</p>
     </div>
     <div v-else>
-      <p>You have: {{tarefas.length}} task from {{ dataStorage.dataInicial }} to {{ dataStorage.dataFinal }}</p>
+      <p>
+        You have: {{ tarefas.length }} task from
+        {{ dataStorage.dataInicial }} to {{ dataStorage.dataFinal }}
+      </p>
       <table class="container">
         <thead>
           <tr>
@@ -27,11 +46,14 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="(tarefa, index) in tarefas" 
-          :key="index">
-            <td>{{tarefa.nome}}</td>
-            <td>{{tarefa.data}}</td>
-            <td><button @click="tarefas.splice(index, 1)" class="btn btn-danger"><i class="bi bi-trash"></i></button></td>
+          <tr v-for="(tarefa, index) in tarefas" :key="index">
+            <td>{{ tarefa.nome }}</td>
+            <td>{{ tarefa.data }}</td>
+            <td>
+              <button @click="tarefas.splice(index, 1)" class="btn btn-danger">
+                <i class="bi bi-trash"></i>
+              </button>
+            </td>
           </tr>
         </tbody>
       </table>
@@ -40,77 +62,98 @@
 </template>
 
 <script>
-import { mapState, mapMutations } from 'vuex'
+import { mapState, mapMutations } from "vuex";
+import copy from "copy-to-clipboard";
 // @ is an alias to /src
 export default {
-  name: 'Home',
+  name: "Home",
   data() {
     return {
       title: "Week task list",
-      inicial: this.$store.state.dataStorage.dataInicial || '',
-      final: this.$store.state.dataStorage.dataFinal || '',
+      inicial: this.$store.state.dataStorage.dataInicial || "",
+      final: this.$store.state.dataStorage.dataFinal || "",
       tarefas: [],
-      tarefa: '',
-      dataInicialFormata: ''
-    }
+      tarefa: "",
+      dataInicialFormata: "",
+      aCopiar: [],
+    };
   },
   computed: {
-    ...mapState(['dataStorage'])
+    ...mapState(["dataStorage"]),
   },
   methods: {
-    ...mapMutations(['GET_PERIODS_DATE']),
+    ...mapMutations(["GET_PERIODS_DATE"]),
     adicionarTarefa() {
-      if(this.inicial === '' && this.final === '') {
-        alert('informe o periodo da semana')
-        return
+      if (this.inicial === "" && this.final === "") {
+        alert("informe o periodo da semana");
+        return;
       }
-      if(this.tarefa === '') {
-        alert('é necessario preencher o campo tarefa')
-        return
+      if (this.tarefa === "") {
+        alert("é necessario preencher o campo tarefa");
+        return;
       }
       function formatDate(date) {
-        if (typeof date === 'string') {
-          date = new Date(...date.split('-'))
+        if (typeof date === "string") {
+          date = new Date(...date.split("-"));
         } else {
-          date = new Date(date)
+          date = new Date(date);
         }
 
-        const day = date.getDate()
-        const month = date.getMonth() + 1
-        const year = date.getFullYear()
+        const day = date.getDate();
+        const month = date.getMonth() + 1;
+        const year = date.getFullYear();
 
-        return `${day}/${month}/${year}`
+        return `${day}/${month}/${year}`;
       }
-      const final = formatDate(this.final)
-      const inicial = formatDate(this.inicial)
+      const final = formatDate(this.final);
+      const inicial = formatDate(this.inicial);
       this.tarefas = [
-        ...this.tarefas, {
-         nome: this.tarefa, 
-         data: formatDate(new Date())
-        }]
-       this.GET_PERIODS_DATE({
+        ...this.tarefas,
+        {
+          nome: this.tarefa,
+          data: formatDate(new Date()),
+        },
+      ];
+      this.GET_PERIODS_DATE({
         inicial: inicial,
-        final: final
-      })
-      this.tarefa = ''
+        final: final,
+      });
+      this.tarefa = "";
     },
-  limparLis() {
-    this.tarefas = []
+    limparLis() {
+      this.tarefas = [];
     },
     clearAll() {
-      this.inicial = ''
-      this.final = ''
-    }
+      this.inicial = "";
+      this.final = "";
+    },
+    downloadList() {
+      // console.log(this.tarefas);
+      try {
+        if (this.tarefa.length !== "") {
+          let valorACopiar = this.tarefas
+            .map(({ nome }, index) => {
+              return `${++index} - ${nome}`;
+              // return this.aCopiar;
+            })
+            .join("\n");
+          // copy(valorACopiar);
+          copy(valorACopiar, {
+            debug: true,
+            message: "Press #{key} to copy",
+          });
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
-  mounted() {
-  },
-  components: {
-  }
-}
+  mounted() {},
+  components: {},
+};
 </script>
 
 <style>
-
 .inputs {
   /* background: blueviolet; */
   padding: 10px 0;
@@ -135,9 +178,13 @@ span {
 }
 
 .clearAll {
-background: rgba(204, 103, 8, 0.788);
-color: #fff;
-font-weight: bold;
+  background: rgba(204, 103, 8, 0.788);
+  color: #fff;
+  font-weight: bold;
+}
+
+.download {
+  background: rgb(220, 220, 220);
 }
 
 .normal {
@@ -150,5 +197,4 @@ font-weight: bold;
   color: rgba(39, 185, 204, 0.856);
   font-size: 30px;
 }
-
 </style>
